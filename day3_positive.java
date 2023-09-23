9f3cdb9f5cc78c35db94410b86044ab1cb7d62fd
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-23 19:27:00 +0530

    Add files via upload

---
+++ day3_positive.java
@@ -0,0 +1,14 @@
+class Positive{
+public static void main(String args[])
+{
+int x=8;
+if(x>=0)
+{
+System.out.println(x+" =is positive");
+}
+else
+{
+System.out.println(x+" =is negative");
+}
+}
+ }
