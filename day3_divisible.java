fe5d2e74526e261266ea2d44c276b8991d85eb72
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-23 19:25:25 +0530

    Add files via upload

---
+++ day3_divisible.java
@@ -0,0 +1,14 @@
+class Divisible{
+public static void main(String args[])
+{
+  int a=8;
+  if(a%3==0 || a%7==0)
+  {
+    System.out.println("number is divisible");
+  }
+else
+  {
+   System.out.println("Number is not divisible");
+  }
+}
+}
