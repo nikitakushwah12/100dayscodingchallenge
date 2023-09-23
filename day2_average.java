7bc4752e44e726b12d96c3078fb0a104c0711547
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-23 19:20:37 +0530

    average.java
    average in java

---
+++ day2_average.java
@@ -0,0 +1,12 @@
+class Average{
+public static void main(String args[])
+{
+   int a = 60;
+   int b = 10;
+   int c = 8;
+   int d = 20;
+   int e = 30;
+   
+   System.out.println("average = "+(a+b+c+d+e)/5);
+}
+}
