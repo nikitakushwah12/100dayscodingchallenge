6505a1dc57bd96782355057e301204d155187bad
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-23 19:21:36 +0530

    swap in java
    swap .java

---
+++ day2_swap.java
@@ -0,0 +1,15 @@
+class Swap{
+public static void main(String args[])
+{
+  int x=20;
+  int y=10;
+  int z;
+  z=x;
+  x=y;
+  y=z;
+  System.out.println("x= "+x);
+  System.out.println("y= "+y);
+
+
+}
+}
