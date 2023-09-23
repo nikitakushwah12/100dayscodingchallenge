f3029fe047684ed87b57d16e796be3edc073e913
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-23 19:23:03 +0530

    Add files via upload

---
+++ day2_Evenodd.java
@@ -0,0 +1,14 @@
+class Evenodd
+{
+ public static void main(String args[])
+ {
+ int a=9;
+ if(a%2==0)
+{
+System.out.println(a+" is even number");
+}
+else
+{
+System.out.println(a+" is odd number");
+}
+}
