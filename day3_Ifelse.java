4ab0314950e612af66463c3f87ef1e57f271ecb7
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-23 19:25:49 +0530

    Add files via upload

---
+++ day3_Ifelse.java
@@ -0,0 +1,14 @@
+class Ifelse2{
+public static void main(String args[])
+{
+  int a=6;
+ if(a%5==0)
+{
+  System.out.println(a+" the number is multipal of 5 ");
+}
+else
+{
+  System.out.println(a+" the number is not multipal of 5");
+}
+}
+}
