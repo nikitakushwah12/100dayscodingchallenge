7f133c96173ec5f20f810e666e6a460d04f4e073
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-26 16:40:23 +0530

    Add files via upload

---
+++ Day4_checknumber.java
@@ -0,0 +1,12 @@
+class CheckNumber{
+public static void main(String args[]){
+
+	int a=44;
+if(a>=21 && a<=575)
+	
+	System.out.println(" number is valid");
+else
+
+	System.out.println(" number is not valid");
+	}
+}
