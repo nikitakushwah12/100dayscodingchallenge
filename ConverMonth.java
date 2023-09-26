e3eacb1c46fcab180a5d64c95f940fa5dbb37e42
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-26 16:42:03 +0530

    Add files via upload

---
+++ ConverMonth.java
@@ -0,0 +1,7 @@
+class ConvertIntoMonth{
+public static void main(String args[]){
+
+	int age = 6;
+System.out.println("Month="+(age*12));
+	}
+}
---
+++ Countryvote.java
@@ -0,0 +1,10 @@
+class countryVote{
+public static void main(String args[]){
+
+	int year=20;
+	if(year>=18)
+		System.out.print("yes");
+	else
+		System.out.print("No");
+}
+}
