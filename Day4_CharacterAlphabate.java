fcb08b1a5b5ae0270a340b4e5ae6efe7c5cb0487
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-26 16:40:05 +0530

    Add files via upload

---
+++ Day4_CharacterAlphabate.java
@@ -0,0 +1,10 @@
+class CharacterAlphabate{
+public static void main(String args[]){
+
+	char value = 65;
+if(value>=97 && value<=122 || value>=65 && value<=90)
+	System.out.println("given number is alphabate");
+else
+	System.out.println("given number is not alphabate");
+	}
+}
