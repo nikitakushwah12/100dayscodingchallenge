e4a9bc2d4a765c38ce4fe3d35a042771bf551d43
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-09-26 17:04:00 +0530

    Add files via upload

---
+++ Day5_Lastdigit.java
@@ -0,0 +1,8 @@
+import java.util.scanner;
+public class FindLastDigit {
+  System.out.println("please enter a number : ");
+Scanner sc =new Scanner(System.in);
+ int num =sc.next();
+  System.out.println("The last digit number is: "+num%10);
+}
+}
