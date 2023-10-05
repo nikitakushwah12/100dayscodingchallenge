69d894fcd63da134ef05fd07cf13312fab4bb9b3
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-10-05 15:58:23 +0530

    Add files via upload

---
+++ day14_gradeofstudent.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class Studentgrade{
+public static void main(String args[])
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println(" ENTER YOUR MARKS -->>");
+	int mark1 = sc.nextInt();
+if(mark1>76 && mark1<=100)
+	System.out.println("Grade is A -->>");
+else if(mark1>41 && mark1<=75)
+	System.out.println("Grade is B -->>");
+else
+	System.out.println("Grade is C -->>");
+	
+	System.out.println(" ENTER YOUR MARKS -->>");
+	int mark2 = sc.nextInt();
+if(mark2>76 && mark2<=100)
+	System.out.println("Grade is A -->>");
+else if(mark2>41 && mark2<=75)
+	System.out.println("Grade is B -->>");
+else
+	System.out.println("Grade is C -->>");
+
+	System.out.println(" ENTER YOUR MARKS -->>");
+	int mark3 = sc.nextInt();
+if(mark3>76 && mark3<=100)
+	System.out.println("Grade is A -->>");
+else if(mark3>41 && mark3<=75)
+	System.out.println("Grade is B -->>");
+else
+	System.out.println("Grade is C -->>");
+
+	System.out.println(" ENTER YOUR MARKS -->>");
+	int mark4 = sc.nextInt();
+if(mark4>76 && mark4<=100)
+	System.out.println("Grade is A -->>");
+else if(mark4>41 && mark4<=75)
+	System.out.println("Grade is B -->>");
+else
+	System.out.println("Grade is C -->>");
+
+	System.out.println(" ENTER YOUR MARKS -->>");
+	int mark5 = sc.nextInt();
+if(mark5>76 && mark5<=100)
+	System.out.println("Grade is A -->>");
+else if(mark5>41 && mark5<=75)
+	System.out.println("Grade is B -->>");
+else
+	System.out.println("Grade is C -->>");
+	}
+}
+
+	
