c816467a0ceeb7cfeeb7714792b762f6af8b9eac
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-10-05 15:59:07 +0530

    Add files via upload

---
+++ day15_naturalnum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class NATURALNUM{
+public static void main(String args[])
+{
+	int i;
+	System.out.println("The first 10 natural number are ->\n");
+	for (i=1; i<=10; i++)
+	{
+		System.out.println(i);
+	}
+	System.out.println("\n");
+   }
+}
