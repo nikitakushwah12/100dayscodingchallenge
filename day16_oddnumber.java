4158cf123157a9bbc9d546a3d53e0623cdb026f5
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-10-06 23:58:51 +0530

    Add files via upload

---
+++ day16_oddnumber.java
@@ -0,0 +1,13 @@
+
+class OddNumber
+{
+ public static void main(String args[])
+{
+ System.out.println("The first 10 odd numbers are <<<...");
+ for(int i = 1; i <= 10; i++)
+{
+  System.out.println(2*i-1);
+ }
+}
+}
+  
