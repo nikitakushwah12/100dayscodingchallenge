e0a5ee9fc0c4b9ff0c243d83e34f9c49ef4202c8
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-10-07 14:49:17 +0530

    Add files via upload

---
+++ day17_SumOfNaturalNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class SUMOFNATURALNUMBER
+{
+public static void main(String args[])
+{
+int i, num = 10,sum=0;
+for(i=1;i<=num;++i)
+{
+sum=sum+i;
+}
+System.out.println("Sum of first 10 Natural Numbers is= "+sum);
+}
+}
