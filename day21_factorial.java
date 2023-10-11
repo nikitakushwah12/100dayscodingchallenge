2c6a19ad9c90197382f8ba909803eb7c5b7d30a7
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-10-12 01:27:39 +0530

    Add files via upload

---
+++ day21_factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Factorial
+{
+public static void main(String[]args){
+int i, factorial=1, number;
+System.out.println("Enter the number to which you need to find the factorial<<....");
+Scanner sc = new Scanner(System.in);
+number =sc.nextInt();
+
+for( i = 1;i<=number;i++)
+{
+factorial = factorial * i;
+}
+System.out.println(" factorial of  the given number is <<... "+factorial);
+}
+}
