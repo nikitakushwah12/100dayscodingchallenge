d40863c8466914f7977c8d9946c2f2ce79cfaa1e
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-10-11 23:47:32 +0530

    Add files via upload

---
+++ day20_sumofmultiples.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class SUMOFMULTIPLES
+{
+public static void main(String args[])
+{ 
+int i,sum=0,n;
+for(i=1;i<=40;i++)
+{
+
+n=i*3;
+sum=sum+i;
+
+}
+System.out.println("sum="+sum);
+System.out.println("sum of multiples of 3 from 1 to 40 <<<....");
+System.out.println(sum);
+}}
