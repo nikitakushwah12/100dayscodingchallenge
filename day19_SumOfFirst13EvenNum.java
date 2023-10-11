5c925a948f071d7e8f148519f53100937af14a13
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-10-11 23:46:13 +0530

    Add files via upload

---
+++ day19_SumOfFirst13EvenNum.java
@@ -0,0 +1,15 @@
+class Even13Num
+{
+public static void main(String args[])
+{
+System.out.println("First 13 even numbers sum");
+int i,n,sum=0;
+for(i=0;i<=12;i++)
+{
+  n=2*i;
+sum+=n;
+
+}
+  System.out.println(sum);
+}
+}
