8ee7fbb9d903a4c5f9639a976c0f4c5fbdad8a0f
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-10-13 20:16:48 +0530

    Add files via upload

---
+++ day22_square.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Squareofnumber
+{
+public static  void main(String args [])
+{
+int square=1;
+for(int i=1; i<=12; i++)
+{
+square=i*i;
+System.out.println("square of "+i+" is <<<.... "+square);
+}
+}
+}
+ 
