07495acbf0a5c9c6ce0a327e07cd85b62097d961
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-11-02 13:56:34 +0530

    Add files via upload

---
+++ day28_Armstrongnum.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class ArmStrongNumber{
+public static void main(String args[])
+{
+ Scanner sc=new Scanner(System.in);
+ System.out.println("Enter the number -> ");
+ int n = sc.nextInt();
+ int digit=0 , d,revers=1,sum=0,n1=n;
+while(n!=0)
+{
+ d=n%10;
+ revers=revers*10+d;
+ n=n/10;
+ ++digit;
+}
+
+for(int i=1; i<=digit; i++)
+{
+ d=revers%10;
+int res =1;
+for(int j=1; j<=digit; j++)
+{
+ res=res*d;
+}
+revers=revers/10;
+System.out.print(res);
+sum=sum+res;
+}
+System.out.println("sum is "+sum);
+if(sum==n1)
+{
+ System.out.println(n1+" =number is Armstrong");
+}
+else
+{
+ System.out.println(n1+" =number is not Armstrog");
+}
+}
+}
