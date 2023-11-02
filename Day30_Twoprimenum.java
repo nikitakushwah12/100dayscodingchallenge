1e51f9c39b6cdc415b321dc14e804102a47d77a4
Nikita Kushwah <145590893+nikitakushwah12@users.noreply.github.com>
2023-11-02 14:00:03 +0530

    Add files via upload

---
+++ Day30_Twoprimenum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class twoprimenumrang{
+public static void main(String args[])
+{
+ Scanner sc = new Scanner(System.in);
+ System.out.print("Enter the first number-> ");
+ int n1 = sc.nextInt();
+ System.out.print("Enter the second number-> ");
+ int n2 = sc.nextInt();
+ int i,j;
+for(i=n1; i<=n2; i++)
+{
+ for(j=2; j<=i; j++)
+{
+  
+   if(i%j==0)
+   break;
+
+}
+if(i==j)
+System.out.print(j+" ");
+}
+}
+}
